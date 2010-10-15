3958bf3155c69ea56fa06d6f2b5cb4d242be7f73
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-10-15 11:48:11 +0900

    updated

--- server.pl
+++ server.pl
@@ -2,25 +2,27 @@ use strict;
 use warnings;
 use Plack::Builder;
 use Plack::App::File;
-use HTTP::Server::PSGI;
 use Path::Class;
+use Plack::Loader;
 
 my $ROOT = '.';
 
 do 'regen-index.pl';
 my $file = Plack::App::File->new({ root => '.' });
-my $server = HTTP::Server::PSGI->new();
-$server->run(sub {
-    my $env = shift;
-    my $path = $env->{PATH_INFO};
-    TGP->main();
-    if (-d dir($ROOT, $path)) {
-        if ($path !~ m{/$}) {
-            return [302, [Location => "$path/"], []];
-        } else {
-            $path .= "/index.html";
+Plack::Loader->auto(@ARGV)->run(
+    sub {
+        my $env  = shift;
+        my $path = $env->{PATH_INFO};
+        TGP->main();
+        if ( -d dir( $ROOT, $path ) ) {
+            if ( $path !~ m{/$} ) {
+                return [ 302, [ Location => "$path/" ], [] ];
+            }
+            else {
+                $path .= "/index.html";
+            }
         }
+        return $file->call( { %$env, PATH_INFO => $path } );
     }
-    return $file->call({%$env, PATH_INFO => $path});
-});
+);
 

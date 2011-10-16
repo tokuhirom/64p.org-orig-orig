a5759803aec97d8bccd59b5ea2aa46e2cebea79d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-10-16 16:13:35 +0900

    fill title tag

--- regen-index.pl
+++ regen-index.pl
@@ -5,6 +5,9 @@ use warnings;
 use Text::MicroTemplate ':all';
 use File::Basename;
 
+$|++;
+binmode STDOUT, ':utf8';
+
 my $tmpl = <<'...';
 <!doctype html>
 ? my $files = shift;
@@ -25,6 +28,22 @@ tokuhirom's slide
 
 sub main {
     my @files = files();
+
+    for my $file (@files) {
+        my $htmlfile = "talks/$file/index.html";
+        next unless -f $htmlfile;
+
+        my ($fname) = glob("talks/$file/*.txt");
+        $fname or die "Missing txt file in $file";
+        open my $fh, '<:utf8', $fname or die "Cannot open file $fname: $!";
+        my $title = <$fh>;
+        $title =~ s{^TITLE::}{};
+        $title =~ s/\n$//;
+        $title =~ s/^\x{FEFF}//;
+        print "$title talks/$file\n";
+        replace_title($htmlfile, $title);
+    }
+
     my $result = render_mt($tmpl, \@files)->as_string;
     open my $fh, '>', 'index.html';
     print {$fh} $result;
@@ -37,6 +56,18 @@ sub files {
         push @f, basename($f);
     }
     reverse sort { $a cmp $b } @f;
+}
+
+sub replace_title {
+    my ($filename, $title) = @_;
+    open my $ifh, '<:utf8', $filename or die "Cannot open $filename: $!";
+    my $src = do { local $/; <$ifh> };
+    $src =~ s!<title>.+?</title>!<title>$title</title>!;
+    close $ifh;
+
+    open my $ofh, '>:utf8', $filename or die "Cannot open $filename: $!";
+    print {$ofh} $src;
+    close $ofh;
 }
 
 if ($0 eq __FILE__) {
